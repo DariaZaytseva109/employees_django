cdf756f6a5b8069da7b2bad719c9401435546b43
DariaZaytseva109 <dawazai@mail.ru>
2023-11-01 11:23:24 +0100

    модели и миграции

---
+++ company/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from company.views import validate_phone
+
+
+class Employee(models.Model):
+    fullname = models.CharField(max_length=30)
+    position = models.CharField(max_length=30)
+    phone_number = models.CharField(
+        max_length=30,
+        validators=[validate_phone],
+        blank=True
+    )
+    birth_date = models.DateField(blank=True)
+    email = models.EmailField(blank=True)
+    department = models.ForeignKey('Department',
+                                   on_delete=models.CASCADE)
+
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=30)
+    floor = models.IntegerField
+    branch = models.ForeignKey('Branch', null=True, on_delete=models.SET_NULL)
+
+
+class Branch(models.Model):
+    address = models.CharField(max_length=30)
+    branch_name = models.CharField(max_length=30)
---
+++ company/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+
+def validate_phone(value):
+    if len(value) != 12 or value [:2] != '+7' or not value[1:].isdigit():
+        raise ValidationError(
+            _("%(value)s is not an even number"),
+            params={"value": value}
+        )
+
